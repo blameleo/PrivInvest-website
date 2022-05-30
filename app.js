6e3796f859f4a57c81f91ea667f5b0540f776a6b
Leonard Adjei <82063118+blameleo@users.noreply.github.com>
2022-05-30 16:47:49 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+
+let icon = document.querySelector('.icon')
+
+icon.addEventListener('click', () => {
+    let x = document.querySelector(".list-container");
+    x.classList.toggle("show");
+})
+   
+  
